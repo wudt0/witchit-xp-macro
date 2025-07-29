277e1492089b4ee0865982799ee0b443d2e91959
wudt0 <135972038+wudt0@users.noreply.github.com>
2025-07-29 12:39:13 +0200

    feat. images

--- main.py
+++ main.py
@@ -15,13 +15,25 @@ def random_movement():
 
 def random_rotation():
     """Simulates random rotation by moving the mouse horizontally."""
-    x_offset = np.random.randint(-500, 500)
-    pydirectinput.moveRel(x_offset, 0, duration=1.2, relative=True)
+    x_offset = np.random.randint(-200, 200)
+    pydirectinput.moveRel(x_offset, 0, duration=0.8, relative=True)
 
 
 def random_jump():
     """Simulates a jump by pressing the spacebar."""
     pydirectinput.press('space')
+
+
+def random_left_click():
+    """Simulates a random left click."""
+    pydirectinput.click(button='left')
+    time.sleep(np.random.uniform(0.2, 1))
+
+
+def random_q_right_click():
+    """Simulates pressing 'q' and then right-clicking."""
+    pydirectinput.press('q')
+    pydirectinput.click(button='right')
 
 
 def click_ready_button():
@@ -34,8 +46,10 @@ def click_ready_button():
         print("Ready button not found (confidence too low or not present).")
 
 
+last_q_right_click_time = time.time()
 while True:
     click_ready_button()
+    random_left_click()
     action = np.random.choice(
         ['move', 'rotate', 'jump'],
         p=[0.48, 0.48, 0.04]  # 48% move, 48% rotate, 4% jump
@@ -46,4 +60,9 @@ while True:
         random_rotation()
     elif action == 'jump':
         random_jump()
+
+    if time.time() - last_q_right_click_time > np.random.uniform(15, 23):
+        random_q_right_click()
+        last_q_right_click_time = time.time()
+
     time.sleep(np.random.uniform(0.1, 0.5))
