d747f9a0d716881eeca19ac822659af8917a3d6a
wudt0 <135972038+wudt0@users.noreply.github.com>
2025-07-29 09:46:14 +0200

    fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pydirectinput
 import time
 import numpy as np
 import pyautogui
+from pyautogui import ImageNotFoundException
 
 def random_movement():
     """Simulates random movement by pressing W, A, S, or D for a random duration."""
@@ -15,7 +16,7 @@ def random_movement():
 def random_rotation():
     """Simulates random rotation by moving the mouse horizontally."""
     x_offset = np.random.randint(-200, 200)
-    pydirectinput.moveRel(x_offset, 0, duration=1.7, relative=True)
+    pydirectinput.moveRel(x_offset, 0, duration=0.8, relative=True)
 
 
 def random_jump():
@@ -24,10 +25,13 @@ def random_jump():
 
 
 def click_ready_button():
-    """Detects and clicks the 'Ready' button if it appears on the screen."""
-    location = pyautogui.locateCenterOnScreen('ready.png', confidence=0.8)
-    if location:
-        pyautogui.click(location)
+    """Detects and clicks the 'Ready' button if it appears on the screen with enough confidence."""
+    try:
+        location = pyautogui.locateCenterOnScreen('ready.png', confidence=0.9)
+        if location:
+            pyautogui.click(location)
+    except ImageNotFoundException:
+        print("Ready button not found (confidence too low or not present).")
 
 
 while True:
